d6f837d33be9c7a06665e6f8797d777a5119a261
Oliver Nybroe <olivernybroe@gmail.com>
2017-04-27 12:18:21 +0200

    implemented product batch

--- src/dao/SQL/SQLProductBatch.java
+++ src/dao/SQL/SQLProductBatch.java
@@ -1,29 +1,72 @@
 package dao.SQL;
 
+import connectors.Connector;
 import connectors.DALException;
 import dao.contracts.ProductBatchDAO;
+import dto.Operator;
 import dto.ProductBatch;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SQLProductBatch implements ProductBatchDAO {
 	@Override
-	public ProductBatch find(int pbId) throws DALException {
-		return null;
+	public ProductBatch find(int id) throws DALException {
+		ResultSet rs = Connector.doQuery("SELECT id, status, receiptId FROM productBatch WHERE id = " + id);
+
+		try {
+			if (!rs.first()) throw new DALException("Product batch with id="+id+" does not exist.");
+			return new ProductBatch(
+					rs.getInt("id"),
+					rs.getInt("status"),
+					rs.getInt("receiptId")
+			);
+		} catch (SQLException e) {
+			throw new DALException(e);
+		}
 	}
 
 	@Override
 	public List<ProductBatch> all() throws DALException {
-		return null;
+		List<ProductBatch> list = new ArrayList<>();
+		ResultSet rs = Connector.doQuery("SELECT id, status, receiptId FROM productBatch");
+		try {
+			while (rs.next()) {
+				list.add(
+						new ProductBatch(
+								rs.getInt("id"),
+								rs.getInt("status"),
+								rs.getInt("receiptId")
+						)
+				);
+			}
+		} catch (SQLException e) {
+			throw new DALException(e);
+		}
+		return list;
 	}
 
 	@Override
-	public void create(ProductBatch produktbatch) throws DALException {
-
+	public void create(ProductBatch productBatch) throws DALException {
+		Connector.doUpdate(
+				String.format("CALL createProductBatch(%d, %d, %d);",
+						productBatch.getId(),
+						productBatch.getStatus(),
+						productBatch.getReceiptId()
+				)
+		);
 	}
 
 	@Override
-	public void update(ProductBatch produktbatch) throws DALException {
-
+	public void update(ProductBatch productBatch) throws DALException {
+		Connector.doUpdate(
+				String.format("CALL updateProductBatch(%d, %d, %d);",
+						productBatch.getId(),
+						productBatch.getStatus(),
+						productBatch.getReceiptId()
+				)
+		);
 	}
 }
